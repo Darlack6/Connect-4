f2187904f3c41818f6e2a80c75ad1af114f3a3f5
Darlack6 <78220630+Darlack6@users.noreply.github.com>
2024-05-28 09:44:38 -0400

    fixed rows and cols

--- connect4.java
+++ connect4.java
@@ -6,67 +6,79 @@ import java.io.*;
 import javax.imageio.*;
 
 public class connect4{
-	
 	// Main
 	public static void main(String args[]){
-		connect4 x = new connect4();
-		x.resetBoard();
-	}
-	
-	// MODEL
-	public static void resetBoard(){
-		String [][] board = new String [7][6];
-		for(int i = 0; i < 6; i++){
-			System.out.print("\n");
-			for(int a = 0; a < 7; a++){
-				board [a][i] = null;
-				System.out.print("x");
-			}
-		} 
-	}
-	
-	//square variable tells us what color the player is (e.g. are they x or o)
-	public static boolean winCheck(String [][] board, int introw, int intcol, String square){
-		boolean blnwin;
+		String[][] board = new String[7][6];
+        board = loadBoard();
+		board[6][0]="x";
+		board[6][1]="x";
+		board[6][2]="x";
+        for(int intRow=0;intRow<7;intRow++){
+            for(int intCol=0;intCol<6;intCol++){
+                System.out.print(board[intRow][intCol]);
+            }
+            System.out.println("\n");
+        }
 		
-		if(board[intcol-3][introw] == square && board[intcol-2][introw] == square && board[intcol-1][introw] == square){
-			blnwin = true;
-		}else if(board[intcol-2][introw] == square && board[intcol-1][introw] == square && board[intcol+1][introw] == square){
-			blnwin = true;
-		}else if(board[intcol-1][introw] == square && board[intcol+1][introw] == square && board[intcol+2][introw] == square){
-			blnwin = true;
-		}else if(board[intcol+1][introw] == square && board[intcol+2][introw] == square && board[intcol+3][introw] == square){
-			blnwin = true;
-		}else if(board[intcol][introw-3] == square && board[intcol][introw-2] == square && board[intcol][introw-1] == square){
-			blnwin = true;
-		}else if(board[intcol][introw-2] == square && board[intcol][introw-1] == square && board[intcol][introw+1] == square){
-			blnwin = true;
-		}else if(board[intcol][introw-1] == square && board[intcol][introw+1] == square && board[intcol][introw+2] == square){
-			blnwin = true;
-		}else if(board[intcol][introw+1] == square && board[intcol][introw+2] == square && board[intcol][introw+3] == square){
-			blnwin = true;
-		}else if(board[intcol][introw+1] == square && board[intcol][introw+2] == square && board[intcol][introw+3] == square){
-			blnwin = true;
-		}else if(board[intcol-3][introw+3] == square && board[intcol-2][introw+2] == square && board[intcol-1][introw+1] == square){
-			blnwin = true;
-		}else if(board[intcol-2][introw+2] == square && board[intcol-1][introw+1] == square && board[intcol+1][introw-1] == square){
-			blnwin = true;
-		}else if(board[intcol-1][introw+1] == square && board[intcol+1][introw-1] == square && board[intcol+2][introw-2] == square){
-			blnwin = true;
-		}else if(board[intcol+1][introw-1] == square && board[intcol+2][introw-2] == square && board[intcol+3][introw-3] == square){
-			blnwin = true;
-		}else if(board[intcol-3][introw-3] == square && board[intcol+2][introw+2] == square && board[intcol+1][introw+1] == square){
-			blnwin = true;
-		}else if(board[intcol+2][introw+2] == square && board[intcol+1][introw+1] == square && board[intcol-1][introw-1] == square){
-			blnwin = true;
-		}else if(board[intcol+1][introw+1] == square && board[intcol-1][introw-1] == square && board[intcol-2][introw-2] == square){
-			blnwin = true;
-		}else if(board[intcol-1][introw-1] == square && board[intcol-2][introw-2] == square && board[intcol-3][introw-3] == square){
-			blnwin = true;
-		}else{
-			blnwin = false;
-		}
-		return blnwin;
+		System.out.println(winCheck(board, 6, 3, "x"));
 	}
 
+	public static String[][] loadBoard(){
+        String strBoard[][] = new String[7][6];
+        for(int intCol=0;intCol<7;intCol++){
+            for(int intRow=0;intRow<6;intRow++){
+                 strBoard[intCol][intRow]=null;
+            }
+        }
+        return strBoard;
+    }
+	
+	//square variable tells us what color the player is (e.g. are they x or o)
+	public static boolean winCheck(String [][] board, int intcol, int introw, String square){
+		boolean blnwin;
+		try{
+			if(board[introw-3][intcol] == square && board[introw-2][intcol] == square && board[introw-1][intcol] == square){
+				blnwin = true;
+			}else if(board[introw-2][intcol] == square && board[introw-1][intcol] == square && board[introw+1][intcol] == square){
+				blnwin = true;
+			}else if(board[introw-1][intcol] == square && board[introw+1][intcol] == square && board[introw+2][intcol] == square){
+				blnwin = true;
+			}else if(board[introw+1][intcol] == square && board[introw+2][intcol] == square && board[introw+3][intcol] == square){
+				blnwin = true;
+			}else if(board[introw][intcol-3] == square && board[introw][intcol-2] == square && board[introw][intcol-1] == square){
+				System.out.println("you suck");
+				blnwin = true;
+			}else if(board[introw][intcol-2] == square && board[introw][intcol-1] == square && board[introw][intcol+1] == square){
+				blnwin = true;
+			}else if(board[introw][intcol-1] == square && board[introw][intcol+1] == square && board[introw][intcol+2] == square){
+				blnwin = true;
+			}else if(board[introw][intcol+1] == square && board[introw][intcol+2] == square && board[introw][intcol+3] == square){
+				blnwin = true;
+			}else if(board[introw][intcol+1] == square && board[introw][intcol+2] == square && board[introw][intcol+3] == square){
+				blnwin = true;
+			}else if(board[introw-3][intcol+3] == square && board[introw-2][intcol+2] == square && board[introw-1][intcol+1] == square){
+				blnwin = true;
+			}else if(board[introw-2][intcol+2] == square && board[introw-1][intcol+1] == square && board[introw+1][intcol-1] == square){
+				blnwin = true;
+			}else if(board[introw-1][intcol+1] == square && board[introw+1][intcol-1] == square && board[introw+2][intcol-2] == square){
+				blnwin = true;
+			}else if(board[introw+1][intcol-1] == square && board[introw+2][intcol-2] == square && board[introw+3][intcol-3] == square){
+				blnwin = true;
+			}else if(board[introw-3][intcol-3] == square && board[introw+2][intcol+2] == square && board[introw+1][intcol+1] == square){
+				blnwin = true;
+			}else if(board[introw+2][intcol+2] == square && board[introw+1][intcol+1] == square && board[introw-1][intcol-1] == square){
+				blnwin = true;
+			}else if(board[introw+1][intcol+1] == square && board[introw-1][intcol-1] == square && board[introw-2][intcol-2] == square){
+				blnwin = true;
+			}else if(board[introw-1][intcol-1] == square && board[introw-2][intcol-2] == square && board[introw-3][intcol-3] == square){
+				blnwin = true;
+			}else{
+				blnwin = false;
+			}
+			return blnwin;
+		}catch(ArrayIndexOutOfBoundsException e){
+			blnwin = false;
+			return blnwin;
+		}
+	}
 }
