49f85cf3e7f612e7d154ccbfd0488441559d2416
addetude <addelue@gmail.com>
2024-06-14 16:33:08 -0400

    ADDDED

--- playpanel.java
+++ playpanel.java
@@ -25,11 +25,8 @@ public class playpanel extends JPanel{
     BufferedImage imgp1; 
     BufferedImage imgp2; 
     
-    int intCol;
-    int intRow;
-    int intfall;
-    int intC;
-    boolean blnloop = false;
+    int intX, intY, intRow, intCol, intCheckerX, intCheckerY;
+    boolean blnAnimDone = false;
     
 
     /**methods*/
@@ -37,16 +34,26 @@ public class playpanel extends JPanel{
         super.paintComponent(g);
         g.drawImage(imgbg,0,0,null);
         g.drawImage(imgboard,100,100,null);
-        
-        for(intRow = 0; intRow < 6; intRow++){
-			for(intCol = 0; intCol < 7; intCol++){
-				if(mainGame.board[intRow][intCol] == "x"){
-					g.drawImage(imgp1, intCol*100 + 125, (5-intRow)*-80+520, null);
-				}else if(mainGame.board[intRow][intCol] == "o"){
-					g.drawImage(imgp2, intCol*101 + 125, (5-intRow)*-80+520, null);
-				}
-			}
-		}
+        g.drawImage(imgp1,intCheckerX,intCheckerY,null);
+
+        if(mainGame.board[intRow][intCol] == "x"){
+            g.drawImage(imgp1, intX * 100 + 125, intY*10 + 50, null);
+        }else if(mainGame.board[intRow][intCol] == "o"){
+            g.drawImage(imgp2, intX * 100 + 125, intY*10 + 50, null);
+        }
+
+        if(blnAnimDone == true){
+            for(int intRow = 0; intRow < 6; intRow++){
+                for(int intCol = 0; intCol < 7; intCol++){
+                    if(mainGame.board[intRow][intCol] == "x"){
+                        g.drawImage(imgp1, intCol*101 + 125, (5-intRow)*-80+520, null);
+                    }else if(mainGame.board[intRow][intCol] == "o"){
+                        g.drawImage(imgp2, intCol*101 + 125, (5-intRow)*-80+520, null);
+                    }
+                }
+            }
+            blnAnimDone = false;
+        }
     }
     
     /**constructor*/
